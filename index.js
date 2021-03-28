bd02934831bdb0526c2d568b6e1abf30f5bdf025
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-03-28 16:46:48 +0600

    doneeeeeeee

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(cors());
 
 const port = 8000;
 
+app.get('/', (req, res) =>{
+  res.send("Hello, it's working")
+})
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lkoox.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
